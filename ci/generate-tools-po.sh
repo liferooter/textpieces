0f202c1dfe134bcef657eca60aca1bbb3617350f
Gleb Smirnov <glebsmirnov0708@gmail.com>
2022-09-20 08:52:48 +0300

    license: add license headers for .blp files and CI script

--- ci/generate-tools-po.sh
+++ ci/generate-tools-po.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# SPDX-FileCopyrightText: 2022 Gleb Smirnov <glebsmirnov0708@gmail.com>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 cd "$(git rev-parse --show-toplevel)" || exit 1
 
 jq '.tools[] | .name, .description, .args[]?' data/tools.json |
